46a260b54b66676ad6249c32361f1079185f4014
mohamed sanib <121166817+mohamedsanib@users.noreply.github.com>
2026-03-15 05:16:05 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@
 // export const BASE_URL = "http://localhost:5000";
 // export const API_URL = `${BASE_URL}/api`;
 // export const SHORT_URL_BASE = `${BASE_URL}/r/`;
-export const GOOGLE_CLIENT_ID = "589128319480-1oon76kg2856ghqdm5mrp590eeq8g364.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID = "582112950722-h251gq2odo54ar55i6netpbsa95fh7m9.apps.googleusercontent.com";
 
 export const BASE_URL = "https://api.l.jo3.org";
 export const API_URL = `${BASE_URL}/api`;
